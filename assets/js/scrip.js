71248c616b06e6df251cea1c6ce6057e6ee91ad9
felipe rodriguez <felipe.rodriguezri@gmail.com>
2024-03-03 23:56:04 -0300

    se inlcuye codigo para habilitar los tooltip y popover de boostrap en la hoja script.js

---
+++ assets/js/scrip.js
@@ -0,0 +1,12 @@
+// Habilitar tooltip Boostrap
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// Habilitar popover Boostrap
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Programación Smooth Scroll
+
+
+// Fin Smoot Scroll
